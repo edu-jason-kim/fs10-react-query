875faeeaaf2dd6dd243afeec083fb543aabcdd38
edu-jason-kim <edu.jason.kim@gmail.com>
2025-12-17 13:30:36 +0900

    mutation 마무리

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Link, useParams } from "react-router";
 import { createSizeReview } from "../api";
 import SizeReviews from "../components/SizeReivews";
 import useProductDetailQuery from "../queries/productDetail";
 import useSizeReviewsQuery from "../queries/sizeReviews";
 import { useState } from "react";
+import QUERY_KEYS from "../contants/query-keys";
 
 // /products/:id
 function ProductDetailPage() {
@@ -15,12 +16,27 @@ function ProductDetailPage() {
     height: "",
     fit: "good",
   });
+  // 모든 캐시를 관리하는 queryClient 호출
+  const queryClient = useQueryClient();
 
   const { data, isPending, error } = useProductDetailQuery(productId);
   const { data: reviews } = useSizeReviewsQuery(productId);
 
   const createReviewMutation = useMutation({
     mutationFn: (reviewData) => createSizeReview(reviewData),
+    // mutation이 성공하면 실행하는 함수
+    onSuccess: () => {
+      // review에 대한 query를 무효화 -> review query가 refetch
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.SIZE_REVIEWS, productId],
+      });
+    },
+    onError: () => {
+      // 에러 발생 시 처리할 로직: ex) toast ui 표시
+    },
+    onSettled: () => {
+      // 성공/실패 여부와 상관없이, 요청이 끝나면 실행하는 함수
+    },
   });
 
   // 입력 변경될 때
